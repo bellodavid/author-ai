124b2bca05955b77f62f93c48c2d849ce23bd138
bob <162284281+Brainydel@users.noreply.github.com>
2024-09-25 08:41:08 +0100

    author-ai

---
+++ components/Documents.tsx
@@ -0,0 +1,9 @@
+function Documents() {
+  return (
+    <div className="flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-start rounded gap-5 max-w-7xl mx-auto">
+      Documents
+    </div>
+  );
+}
+
+export default Documents;
